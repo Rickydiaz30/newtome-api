6f5dcc5c3b22971896ebed0cbb950a332ec24b66
Ricky <diazricky28@gmail.com>
2026-03-21 09:15:22 -0400

    feat: add created_at column via Flyway V3 migration

--- src/main/java/com/newtome/newtomeapi/users/repository/UserRepository.java
+++ src/main/java/com/newtome/newtomeapi/users/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.newtome.newtomeapi.users.repository;
 
 import com.newtome.newtomeapi.users.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 
+
+    @Modifying
+    @Query("update User u set u.lastLoginAt = :time where u.username = :username")
+    int updateLastLoginAt(String username, LocalDateTime time);
 }
